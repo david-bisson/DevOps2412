6110cc993ba1d6b476d057d1b0789ba0a3f812fe
david.bisson <david.bisson@nice.com>
2024-02-25 15:18:32 +0200

    another commit

--- 24.py
+++ 24.py
@@ -1,6 +1,6 @@
 import requests
 
-# response = requests.delete("http://localhost:8080/items/1")
+## response = requests.delete("http://localhost:8080/items/1")
 response = requests.get("http://localhost:8080/items")
 actual = len(response.json())
 print ("actual ", actual)
